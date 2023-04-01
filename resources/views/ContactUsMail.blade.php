845509515510adb69910f5dd08337e60c91dbdf2
‪Taha Al-rehany‬‏ <32350359+taharehany@users.noreply.github.com>
2023-04-01 16:20:31 +0200

    edit email template

--- resources/views/ContactUsMail.blade.php
+++ resources/views/ContactUsMail.blade.php
@@ -142,30 +142,6 @@
       </tr>
       <!-- end logo -->
 
-      <!-- start hero -->
-      <tr>
-         <td align="center" bgcolor="#e9ecef">
-            <!--[if (gte mso 9)|(IE)]>
-            <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
-                <tr>
-                    <td align="center" valign="top" width="600">
-            <![endif]-->
-            <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-               <tr>
-                  <td align="left" bgcolor="#ffffff" style="padding: 36px 24px 0; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; border-top: 3px solid #d4dadf;">
-                     <h1 style="margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px;">New Message</h1>
-                  </td>
-               </tr>
-            </table>
-            <!--[if (gte mso 9)|(IE)]>
-            </td>
-            </tr>
-            </table>
-            <![endif]-->
-         </td>
-      </tr>
-      <!-- end hero -->
-
       <!-- start copy block -->
       <tr>
          <td align="center" bgcolor="#e9ecef">
@@ -186,47 +162,30 @@
                      <table border="0" cellpadding="0" cellspacing="0" width="100%">
                         <tr>
                            <td bgcolor="#ffffff" style="padding: 12px;">
-                              <table border="0" cellpadding="0" cellspacing="0">
-                                 <p>
-                                    Name:
-                                    <a target="_blank" style="background-color: #00aee4; padding: 10px 25px; display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">{{$data['data']['name']}}</a>
-                                 </p>
+                              <p>
+                                 Name:
+                                 <a target="_blank" style="background-color: #00aee4; padding: 5px 15px; display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">{{$data['data']['name']}}</a>
+                              </p>
 
-                                 <br>
-                                 <br>
+                              <br>
 
-                                 <p>
-                                    Phone:
-                                    <a target="_blank" href="{{$data['data']['phone']}}" style="background-color: #00aee4; padding: 10px 25px; display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">{{$data['data']['phone']}}</a>
-                                 </p>
+                              <p>
+                                 Phone:
+                                 <a target="_blank" href="{{$data['data']['phone']}}" style="background-color: #00aee4; padding: 5px 15px; display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">{{$data['data']['phone']}}</a>
+                              </p>
 
-                                 <br>
-                                 <br>
+                              <br>
 
-                                 <p>
-                                    Project Name:
-                                    <a target="_blank" style="background-color: #00aee4; padding: 10px 25px; display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">{{$data['data']['project-name']}}</a>
-                                 </p>
-                              </table>
+                              <p>
+                                 Project Name:
+                                 <a target="_blank" style="background-color: #00aee4; padding: 5px 15px; display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">{{$data['data']['project-name']}}</a>
+                              </p>
                            </td>
                         </tr>
                      </table>
                   </td>
                </tr>
                <!-- end button -->
-
-               <!-- start copy -->
-
-               <!-- end copy -->
-
-               <!-- start copy -->
-               <tr>
-                  <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; border-bottom: 3px solid #d4dadf">
-                     <p style="margin: 0;">https://newhome-re.co,<br></p>
-                  </td>
-               </tr>
-               <!-- end copy -->
-
             </table>
             <!--[if (gte mso 9)|(IE)]>
             </td>
