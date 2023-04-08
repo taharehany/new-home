f00934fe026f13bb015884a0a665d2ebfd737195
‪Taha Al-rehany‬‏ <32350359+taharehany@users.noreply.github.com>
2023-04-08 15:31:33 +0200

    add code to head

--- resources/views/front/layouts/header.blade.php
+++ resources/views/front/layouts/header.blade.php
@@ -24,6 +24,24 @@
    <link href="https://fonts.googleapis.com/css2?family=Tajawal&amp;display=swap" rel="stylesheet">
    <link rel="stylesheet" href="{{ asset('front/css/style.css') }}">
    <link rel="stylesheet" href="{{ asset('front/css/over-style.css') }}">
+
+   <!-- Hotjar Tracking Code for my site -->
+   <script>
+      (function(h, o, t, j, a, r) {
+         h.hj = h.hj || function() {
+            (h.hj.q = h.hj.q || []).push(arguments)
+         };
+         h._hjSettings = {
+            hjid: 3442873,
+            hjsv: 6
+         };
+         a = o.getElementsByTagName('head')[0];
+         r = o.createElement('script');
+         r.async = 1;
+         r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+         a.appendChild(r);
+      })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+   </script>
 </head>
 
 <body>
